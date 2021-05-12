8a4f72586f4e48d996c917b3777bf83f0ccaedbb
Chris Yammine <cyammine91@gmail.com>
2021-05-11 23:53:06 -0700

    Add a batch number to state machine

--- raft/statemachine.go
+++ raft/statemachine.go
@@ -2,9 +2,11 @@ package raft
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math/big"
 
 	sm "github.com/lni/dragonboat/v3/statemachine"
 )
@@ -14,17 +16,22 @@ import (
 // See https://github.com/lni/dragonboat/blob/master/statemachine/rsm.go for
 // more details of the IStateMachine interface.
 type ExampleStateMachine struct {
-	ClusterID uint64
-	NodeID    uint64
-	Count     uint64
+	ClusterID uint64 `json:"-"`
+	NodeID    uint64 `json:"-"`
+
+	Count           uint64
+	NextBatchNumber *big.Int
 }
+
+var _ sm.IStateMachine = (*ExampleStateMachine)(nil)
 
 // NewExampleStateMachine creates and return a new ExampleStateMachine object.
 func NewExampleStateMachine(clusterID uint64, nodeID uint64) sm.IStateMachine {
 	return &ExampleStateMachine{
-		ClusterID: clusterID,
-		NodeID:    nodeID,
-		Count:     0,
+		ClusterID:       clusterID,
+		NodeID:          nodeID,
+		Count:           0,
+		NextBatchNumber: big.NewInt(0),
 	}
 }
 
@@ -43,33 +50,33 @@ func (s *ExampleStateMachine) Update(data []byte) (sm.Result, error) {
 	// incoming update request. we also increase the counter by one to remember
 	// how many updates we have applied
 	s.Count++
-	fmt.Printf("from ExampleStateMachine.Update(), msg: %s, count:%d\n",
-		string(data), s.Count)
+	s.NextBatchNumber = s.NextBatchNumber.Add(s.NextBatchNumber, big.NewInt(1))
+	fmt.Printf("from ExampleStateMachine.Update(), msg: %s, count:%d, next_batch_number: %s\n", string(data), s.Count, s.NextBatchNumber.String())
 	return sm.Result{Value: uint64(len(data))}, nil
 }
 
 // SaveSnapshot saves the current IStateMachine state into a snapshot using the
 // specified io.Writer object.
 func (s *ExampleStateMachine) SaveSnapshot(w io.Writer, fc sm.ISnapshotFileCollection, done <-chan struct{}) error {
-	// as shown above, the only state that can be saved is the Count variable
-	// there is no external file in this IStateMachine example, we thus leave
-	// the fc untouched
-	data := make([]byte, 8)
-	binary.LittleEndian.PutUint64(data, s.Count)
-	_, err := w.Write(data)
+	b, err := json.Marshal(s)
+	if err != nil {
+		return fmt.Errorf("save snapshot json marshal: %w", err)
+	}
+	_, err = w.Write(b)
 	return err
 }
 
 // RecoverFromSnapshot recovers the state using the provided snapshot.
 func (s *ExampleStateMachine) RecoverFromSnapshot(r io.Reader, files []sm.SnapshotFile, done <-chan struct{}) error {
-	// restore the Count variable, that is the only state we maintain in this
-	// example, the input files is expected to be empty
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
-		return err
+		return fmt.Errorf("readall: %w", err)
 	}
-	v := binary.LittleEndian.Uint64(data)
-	s.Count = v
+
+	if err := json.Unmarshal(data, s); err != nil {
+		return fmt.Errorf("recover from snapshot unmarshal json: %w", err)
+	}
+
 	return nil
 }
 
