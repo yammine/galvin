f1470772a3500eb961741d2b2a3b37f12f17f5e2
Chris Yammine <cyammine91@gmail.com>
2021-06-02 22:42:34 -0700

    Add ascii diagram

--- sequencer/reader.go
+++ sequencer/reader.go
@@ -2,7 +2,7 @@ package sequencer
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"time"
 )
 
@@ -25,21 +25,25 @@ func NewReader(s LogStore, scheduler Scheduler) *Reader {
 }
 
 func (r *Reader) Run(ctx context.Context) {
+	reads := 0
 	for {
 		subCtx, cancel := context.WithTimeout(context.Background(), time.Second)
 		// Read a batch from our log store
 		b, err := r.s.SyncRead(subCtx, 1, r.batchNumber)
 		cancel()
+		reads++
 
 		if err != nil {
-			fmt.Println("Error fetching batch", err)
+			if reads%10 == 0 {
+				log.Println("Error fetching batch", err)
+			}
 			time.Sleep(time.Second)
 			continue
 		}
 
 		select {
 		case <-ctx.Done():
-			fmt.Println("ctx cancelled, terminating loop")
+			log.Println("ctx cancelled, terminating loop")
 			return
 		case r.scheduler.BatchInput() <- b.([]Transaction):
 			r.batchNumber++
